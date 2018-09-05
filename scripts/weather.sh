1daa6aa8673991f489ec92e4be9d4aeeccbad986
Anas Aboureada <anas.ieee@gmail.com>
2018-09-05 12:18:30 +0200

    "Fixed some bugs, added new features and added some new bugs!"

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,3 +1,4 @@
 !#/bin/sh
 
-http --json "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=77f3ecc037d5e00e40b5487ca31f7367&units=metric" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'weather'"][0]["'description'"] +", "+ str(obj["'main'"]["'temp'"]) + " - " + str(obj["'main'"]["'temp_min'"]) + "\\" + str(obj["'main'"]["'temp_max'"]) + " ~" + str(obj["'main'"]["'humidity'"])'
+http --json "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=77f3ecc037d5e00e40b5487ca31f7367&units=metric" | \
+python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["'weather'"][0]["'description'"] + " 🌡  " + str(obj["'main'"]["'temp'"]) + " ⬇ " + str(obj["'main'"]["'temp_min'"]) + " ⬆ " + str(obj["'main'"]["'temp_max'"]) + " 💦 " + str(obj["'main'"]["'humidity'"]) + " " + " 🌬 " + str(obj["'wind'"]["'speed'"]))'
